a616ebbb0710ce4bd41508b7ac484f373852aacc
cjtrombley <cjtrombley@oakland.edu>
2017-03-09 21:31:31 -0500

    Initial class setup

---
+++ src/LinkedList.java
@@ -0,0 +1,18 @@
+public class LinkedList {
+	
+	private PersonNode head;
+	
+	
+	public LinkedList() {
+		this.head = null;
+	}
+	
+	//add()
+	
+	//get()
+	
+	//remove()
+	
+	//toString
+
+}
---
+++ src/ProjectLaunch.java
@@ -0,0 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+
+public class ProjectLaunch {
+
+	public static void main(String[] args) {
+	
+	System.out.println("Thank you for using TelephoneBook v1.0");
+	
+	}
+		
+		
+		
+	private void initHash() throws Exception{
+		
+	//String inputFile = args[1]
+	//String outputFile = args[2]
+	
+	//BufferedReader br = new BufferedReader(new FileReader(new File(inputFile)));
+	
+	//String line = br.readLine();
+	}
+	
+}
